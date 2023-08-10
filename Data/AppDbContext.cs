383b61a88554aa70c12ede1638431404ed455c00
Matheus Nogueira <matheus.garcianogueira@hotmail.com>
2023-08-10 16:09:09 -0300

    feat(data): adicionada a classe AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechTestBackendCSharp.Models;
+
+namespace TechTestBackendCSharp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
