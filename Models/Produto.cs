bf2f652ebc8551a9a086189f540b4569eafe3853
Matheus Nogueira <matheus.garcianogueira@hotmail.com>
2023-08-10 16:08:55 -0300

    feat(model): adicionada a classe Produto

---
+++ Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace TechTestBackendCSharp.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int QuantidadeEmEstoque { get; set; }
+        public DateTime DataDeCriacao { get; set; }
+        public decimal ValorTotal => Preco * QuantidadeEmEstoque;
+    }
+}
