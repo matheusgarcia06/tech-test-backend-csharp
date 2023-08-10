9aa003cd072e4d3cb5235948ecc32e95bf7a63c5
Matheus Nogueira <matheus.garcianogueira@hotmail.com>
2023-08-10 16:26:16 -0300

    feat(model): adicionada a classe ProdutoViewModel

---
+++ Models/ProdutoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TechTestBackendCSharp.Models
+{
+    public class ProdutoViewModel
+    {
+        public string? Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int QuantidadeEmEstoque { get; set; }
+    }
+}
