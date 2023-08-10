1416f8e03cd0ed44decfa6129776d55ca6493960
Matheus Nogueira <matheus.garcianogueira@hotmail.com>
2023-08-10 16:27:11 -0300

    feat(services): adicionada a pasta "services" com a interface IProdutoService

---
+++ Services/IProdutoService.cs
@@ -0,0 +1,14 @@
+﻿using System.Transactions;
+using TechTestBackendCSharp.Models;
+
+namespace TechTestBackendCSharp.Services
+{
+    public interface IProdutoService
+    {
+        Task<IEnumerable<Produto>> ObterTodosProdutosAsync();
+        Task<Produto> ObterProdutoPorIdAsync(int id);
+        Task<IEnumerable<Produto>> CadastrarProdutoAsync(ProdutoViewModel novoProduto);
+        Task<Produto> AtualizarProdutoPorIdAsync(int id, ProdutoViewModel produtoAtualizado);
+        Task<Produto> DeletarProdutoPorIdAsync(int id);
+    }
+}
